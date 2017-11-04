e5f09c388301c38f4217fcd93d2ab3e371e14818
afezeria <zodal@outlook.com>
2017-11-04 19:51:08 +0800

    4个

---
+++ answers/156.Map_Defaults.clj
@@ -0,0 +1,11 @@
+(= (___ 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+(fn [x y] 
+  (loop [val (repeat (count y) x) key y newmap {}] 
+    (if-not (> (count val) 0)
+      newmap
+      (recur (rest val) (rest key) (assoc newmap (first key) (first val))))))
+
+;(if-not (> (count val) 0) ) 应该用 (if (empty? val))代替
+;实际上这个匿名函数的功能zipmap已经实现了
+
+#(zipmap %2 (repeat %1))
