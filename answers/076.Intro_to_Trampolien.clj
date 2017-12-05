c99919971b95bf32a5b1ffd92e411bb05c95ca6d
afezeria <zodal@outlook.com>
2017-12-05 21:10:22 +0800

    更新

---
+++ answers/076.Intro_to_Trampolien.clj
@@ -0,0 +1,9 @@
+(= __
+   (letfn
+     [(foo [x y] #(bar (conj x y) y))
+      (bar [x y] (if (> (last x) 10)
+                   x
+                   #(foo x (+ 2 y))))]
+     (trampoline foo [] 1)))
+
+(range 1 12 2)
