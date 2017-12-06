08edb9f04caebe4ce386ca46f133192e1f421812
afezeria <zodal@outlook.com>
2017-12-07 01:43:24 +0800

    更新

--- answers/077.Anagram_Finder.clj
+++ answers/077.Anagram_Finder.clj
@@ -1,2 +1,8 @@
 (= (__ ["meat" "mat" "team" "mate" "eat"])
    #{#{"meat" "team" "mate"}})
+
+(fn [coll]
+  (loop [m {} c coll]
+    (if (nil? (seq c))
+      (set (filter #(> (count %) 1) (vals m)))
+      (recur (merge-with into m {(set (first c)) #{(first c)}}) (rest c)))))
